a0f08ebb448c1a2869ddf5b49d489006bd0645c7
eramos3 <cheddarshreddar3@gmail.com>
2021-02-04 08:17:34 -0800

    fixed seeding comment

--- models/Comment.js
+++ models/Comment.js
@@ -15,22 +15,22 @@ Comment.init(
             type: DataTypes.STRING,
             allowNull: false
         },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'user',
-                key: 'id'
-            }
-        },
-        post_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'post',
-                key: 'id'
-            }
-        }
+        // user_id: {
+        //     type: DataTypes.INTEGER,
+        //     allowNull: false,
+        //     references: {
+        //         model: 'user',
+        //         key: 'id'
+        //     }
+        // },
+        // post_id: {
+        //     type: DataTypes.INTEGER,
+        //     allowNull: false,
+        //     references: {
+        //         model: 'post',
+        //         key: 'id'
+        //     }
+        // }
     },
     {
         sequelize,
