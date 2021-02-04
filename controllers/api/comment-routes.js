bba615737010dd88be5e5e3ed6997885bccd7582
eramos3 <cheddarshreddar3@gmail.com>
2021-02-03 22:23:30 -0800

    added seeds file

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
+
 // get comments api/comments
 router.get("/", (req, res) => {
     Comment.findAll({})
