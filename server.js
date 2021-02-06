571ab9473290be00cc29f49bf82179f8f26d8ebc
eramos3 <cheddarshreddar3@gmail.com>
2021-02-06 10:34:02 -0800

    added homepage/postinfo handlebars

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const path = require('path');
-
-const helpers = require('./utils/helpers');
-
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
-
 const session = require('express-session');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -30,16 +25,16 @@ const sess = {
 
 app.use(session(sess));
 
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', exphbs({defaultLayout: "main"}));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res)=> res.render('main'))
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(routes);
 app.use(require('./controllers/'));
 
 // turn on connection to db and server
