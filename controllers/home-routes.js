35158d4cd0efe423f00299205b768bcc763c671f
ArlonTuazon <nikka1378@yahoo.com>
2021-02-06 12:31:57 -0800

    added logout route and edit main page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -39,6 +39,24 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+
+  router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('signup');
+  });
+
 router.get('/:id', (req, res) => {
     Drawing.findOne({
         where: {
