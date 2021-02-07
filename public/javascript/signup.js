3a49af018b3dbd6a57c577b76d400b62c9ded553
eramos3 <cheddarshreddar3@gmail.com>
2021-02-06 21:19:26 -0800

    updated css

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -19,7 +19,7 @@ async function signupFormHandler(event) {
       // check the response status
       if (response.ok) {
         console.log('success');
-        document.location.replace('/dashboard');
+        document.location.replace('/');
       } else {
         alert(response.statusText);
       }
