b28695a9593ed661c0c62187ebd7e1fbcb4cfdd9
CassBut <clhurlbut@gmail.com>
2021-02-09 21:17:40 -0800

    cassie - sorry, should be no changes

--- public/javascript/canvas.js
+++ public/javascript/canvas.js
@@ -30,31 +30,32 @@
         drawData.innerHTML = "Capture the dataURL";
     }, false);
 
-    async function newFormHandler(event) {
-        event.preventDefault();
-
-        // const draw_title = document.querySelector('input[name="drawing-title"]').value;
-        const image = document.querySelector('input[name="draw_DataURL"]').value;
-
-        const response = await fetch(`/api/drawing`, {
-            method: 'POST',
-            body: JSON.stringify({
-                image
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        } else {
-            alert(response.statusText);
-        }
-    }
     submitBtn.addEventListener("click", function (event) {
         let dataUrl = canvas.toDataURL();
         drawData.innerHTML = dataUrl;
+        async function newFormHandler(event) {
+            event.preventDefault();
+
+            // const draw_title = document.querySelector('input[name="drawing-title"]').value;
+            const image = document.querySelector('input[name="draw_DataURL"]').value;
+
+            const response = await fetch(`/api/drawing`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    user_id,
+                    image
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (response.ok) {
+                document.location.replace('/dashboard');
+            } else {
+                alert(response.statusText);
+            }
+        }
         newFormHandler();
         clearCanvas();
     }, false);
